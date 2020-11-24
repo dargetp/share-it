02c4474c4ffec7a6c88cdc5188f994e6afece166
PatriciaDarget <dargetp@yahoo.fr>
2020-11-24 10:19:41 +0100

    AJOUTE   la config minimale du projet Configure la connexion à la base de données, le template de base Twig et les routes de chargement et téléchargement de fichiers.

--- config/doctrine.php
+++ config/doctrine.php
@@ -7,7 +7,8 @@ return [
     'connection' => [
         // Put your database connection parameters here
         // see the official documentation for more infos
-        // https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html
-        'url' => 'mysql://user:password@localhost:3306/database'
+        // https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html        
+        'url' => 'mysql://root:@localhost:3306/share_it'
+
     ]
 ];
--- public/index.php
+++ public/index.php
@@ -12,11 +12,23 @@ use Slim\App;
 // App configuration
 require_once __DIR__ . '/../config/bootstrap.php';
 
+
 // Application routes
 $app
-    ->get('/', [HomeController::class, 'homepage'])
+    ->map(['GET', 'POST'], '/', [HomeController::class, 'homepage'])
     ->setName('homepage')
 ;
+
+//On peut indiquer des parametres dans les routes entre accolades: {param}
+//On peut indiquer leur format avec des RegEx: \d+ (constitué d'un ou plusieurs chiffres)
+
+//Les paramètres seront envoyés en argument de la méthode du controleur
+$app
+    ->get('/download/{id:\d+}', [HomeController::class, 'download'])
+    ->setName('download')
+
+;
+
 
 // Start the application
 $app->run();
